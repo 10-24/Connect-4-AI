45d5960a431da021df1732bcbd412691c0134cd6
Ryan Dixon <ryan.dixon620@gmail.com>
2025-04-23 16:44:55 -0500

    Bruh

--- src/training/memory/batch.rs
+++ src/training/memory/batch.rs
@@ -1,28 +1,28 @@
 use std::{
-    fs::{self, File},
-    path::Path,
+    fs::{self, File}, io::Write, path::Path
 };
 
 use burn::tensor::{Int, Tensor};
 use chrono::Local;
+use derive_new::new;
 use serde::Serialize;
 
 use crate::{
-    connect_four::game_board::GameBoard, training::{model::target_q_val_builder::TargetQValBuilder, train::BATCH_SIZE},
+    connect_four::game_board::GameBoard,
+    training::{
+        model::target_q_val_builder::TargetQValBuilder,
+        train::{BATCH_SIZE, NUM_BATCHES},
+    },
     Bknd, DEVICE,
 };
 
 use super::episode_memory::TrainingFrame;
 
-#[derive(Debug,Clone)]
+#[derive(Debug, Clone, new, Default)]
 pub struct Batch {
     pub training_frames: Vec<TrainingFrame>,
 }
 impl Batch {
-    pub fn new(training_frames: Vec<TrainingFrame>) -> Self {
-        Self { training_frames }
-    }
-
     pub fn get_game_states(&self) -> Tensor<Bknd, 2> {
         let mut board = GameBoard::new();
 
@@ -64,28 +64,55 @@ impl Batch {
         Tensor::from_floats(state_values.as_slice(), &DEVICE)
     }
 
-    pub fn save(&self, folder: &Path,file_name:&str,) {
-        let timestamp = Local::now().format("%m-%d_%H-%M").to_string();
-        let batch_file_name = format!("Batch_{file_name}_{timestamp}");
-        let batch_file_path = folder.join(batch_file_name);
-
-        let mut csv_writer = csv::Writer::from_path(batch_file_path).unwrap();
-
-        self.training_frames.iter().for_each(|training_frame| {
-            csv_writer
-                .serialize(training_frame)
-                .expect("Err serializing training_frame");
-        });
-        csv_writer.flush().expect("unable to save csv");
+    pub fn to_csv(&self) -> Vec<u8> {
+        let mut csv_writer = csv::Writer::from_writer(Vec::with_capacity(self.training_frames.len()));
+        self.training_frames.iter().for_each(|frame|
+            csv_writer.serialize(frame).expect("Cannot serialize turn")
+        );
+        csv_writer.into_inner().unwrap()
     }
 
     pub fn from_file(path: &Path) -> Self {
-
         let mut csv_reader = csv::Reader::from_path(path).unwrap();
-        let training_frames:Vec<TrainingFrame> = csv_reader.deserialize().map(|frame|
-            frame.expect("Unable to deserialized training frame")
-        ).collect();
-        
+        let training_frames: Vec<TrainingFrame> = csv_reader
+            .deserialize()
+            .map(|frame| frame.expect("Unable to deserialized training frame"))
+            .collect();
+
         Self { training_frames }
+    }
+}
+
+#[derive(Debug)]
+pub struct BatchFile {
+    batches: Vec<Batch>,
+    file_path_string: String,
+}
+impl BatchFile {
+    pub fn new(folder: &str) -> Self {
+        let file_name = Self::create_file_name();
+        let file_path_string = format!("{folder}/{file_name}");
+
+        let batches = Vec::with_capacity(NUM_BATCHES as usize);
+        
+        Self {
+            batches,
+            file_path_string,
+        }
+    }
+    pub fn add(&mut self, batch: Batch) {
+        self.batches.push(batch);
+    }
+    pub fn save(self) {
+        
+        let file = File::create_new(self.file_path_string).expect("Couldn't create a new file");
+        let csv:Vec<u8> = self.batches.iter().map(|batch| 
+            batch.to_csv()
+        ).reduce(|&mut a, &mut b|{}).collect();
+        file.write(buf)
+    }
+    fn create_file_name() -> String {
+        let timestamp = Local::now().format("%m-%d_%H-%M").to_string();
+        format!("Batch_{timestamp}")
     }
 }
--- src/training/train.rs
+++ src/training/train.rs
@@ -1,21 +1,30 @@
-use std::{path::Path, sync::Arc};
-use burn::{module::Module, optim::AdamConfig, record::{DefaultFileRecorder, FullPrecisionSettings, NamedMpkFileRecorder, Recorder}};
+use crate::{
+    training::{
+        memory::convert_episode_to_training_frames::convert_episode_to_training_frames,
+        model::model_config::ModelConfig, optimize_model::optimize_model::optimize_model,
+    },
+    ModelWithBackend, DEVICE,
+};
+use burn::{
+    module::Module,
+    optim::AdamConfig,
+    record::{DefaultFileRecorder, FullPrecisionSettings, NamedMpkFileRecorder, Recorder},
+};
 use rayon::iter::{IntoParallelIterator, ParallelIterator};
-use crate::{training::{memory::convert_episode_to_training_frames::convert_episode_to_training_frames, model::model_config::ModelConfig, optimize_model::optimize_model::optimize_model}, ModelWithBackend, DEVICE};
+use std::{path::Path, sync::Arc};
 
-use super::{memory::batch::Batch, run_episode::run_episode};
+use super::{memory::batch::{Batch, BatchFile}, run_episode::run_episode};
 
 pub const NUM_BATCHES: u16 = 1000;
 pub const BATCH_SIZE: usize = 6;
-pub const SAVE_INTERVAL:u16 = 20;
 
 const EPSILON: f32 = 0.65;
 const GAMMA: f32 = 0.97;
 const MODEL_WEIGHTS_PATH: &str = "model.safetensors";
 
 pub fn train() {
-    
-    let model_parameters_recorder: NamedMpkFileRecorder<FullPrecisionSettings> = DefaultFileRecorder::new();
+    let model_parameters_recorder: NamedMpkFileRecorder<FullPrecisionSettings> =
+    DefaultFileRecorder::new();
     let mut model = ModelConfig::new().init(&DEVICE);
     
     let training_config = TrainingConfig {
@@ -24,14 +33,13 @@ pub fn train() {
         learning_rate: 0.0003,
     };
     let mut optimizer = AdamConfig::new().init();
+    let mut batch_file = BatchFile::new("episode_data");
 
     for i in 1..=NUM_BATCHES {
-        
-        let episodes = (0..BATCH_SIZE).into_par_iter()
-            .map(|_| {
-                let model = ModelConfig::new().init(&DEVICE);
-                run_episode(&model, &training_config)
-            });
+        let episodes = (0..BATCH_SIZE).into_par_iter().map(|_| {
+            let model = ModelConfig::new().init(&DEVICE);
+            run_episode(&model, &training_config)
+        });
 
         let training_frames = episodes
             .map(|episode| convert_episode_to_training_frames(episode, &training_config))
@@ -40,17 +48,17 @@ pub fn train() {
                 reducer
             });
         let batch = Batch::new(training_frames);
-        
 
-        batch.save(Path::new("episode_data"),i.to_string().as_str());
-        
-        let (new_model,loss) = optimize_model(batch.clone(),model,&mut optimizer,&training_config,);
+        let (new_model, loss) =
+            optimize_model(batch.clone(), model, &mut optimizer, &training_config);
+
+            model = new_model;
         println!("batch {i}/{NUM_BATCHES}, loss {loss}");
-        model = new_model;
-        if i % SAVE_INTERVAL == 0 {
-            model.clone().save_file(MODEL_WEIGHTS_PATH, &model_parameters_recorder).expect("Cannot save model parameters");
-        }
+        batch_file.add(batch);
     }
+    
+    batch_file.save();
+    model.save_file(MODEL_WEIGHTS_PATH, &model_parameters_recorder).expect("Unable to save model");
 }
 
 #[derive(Clone, Copy)]
